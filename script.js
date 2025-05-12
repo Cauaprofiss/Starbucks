dde0b028d5db7fabd1dcbd4b9aae4f394be7110c
Cauã Messias <cmessiasdemelosantos@gmail.com>
2025-05-12 16:03:02 -0300

    Primeiro teste

---
+++ script.js
@@ -0,0 +1,13 @@
+let circulo = document.querySelector(".circulo");
+let copo = document.querySelector(".copo");
+
+function clicar(cor){
+    circulo.style.backgroundColor = cor;
+}
+
+function mudarImg(img){
+    copo.src = img;
+}
+
+
+
